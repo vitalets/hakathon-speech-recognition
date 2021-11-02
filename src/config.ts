d7492ca08d9d1286d174eff6c3676eabad06c716
Vitaliy Potapov <noginsk@rambler.ru>
2021-11-02 17:24:45 +0300

    enable mocks

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ const {
 export const config = {
   googleBucket: 'hakathon',
   googleAuthFile: '.auth.google.json',
-  googleUseMocks: false,
+  googleUseMocks: true,
   punctuationApi: 'http://84.252.141.1:5000/restore-punct',
   logLevel: LOG_LEVEL || 'info',
 };
